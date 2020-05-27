fb3d90838b3f3aa85ce8c0c5e386c84c999f1f74
Daniel Scicluna <daniel.scicluna@gig.com>
2020-05-27 19:50:26 +0200

    clean front page file

--- front-page.php
+++ front-page.php
@@ -5,10 +5,3 @@ get_header();
 get_sidebar();
 
 get_footer();
-?>
-<button>hello</button>
-<div class="svg"></div>
-<i class="feature-box__icon icon-basic-test"></i>
-<div class="img"></div>
-<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/src/images/bass.jpg">
-<span class="fontme">hello there!</span>
