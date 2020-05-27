0444e433d84bb985202bb5294d4c19442112f987
Daniel Scicluna <daniel.scicluna@gig.com>
2020-05-27 14:27:57 +0200

    add js and scss files and clean other files

---
+++ assets/js/back/footer.js
@@ -0,0 +1,2 @@
+// Keep module.hot.accept for HMR - This is removed automatically on build
+if (module.hot) module.hot.accept();
---
+++ assets/js/front/footer.js
@@ -0,0 +1,3 @@
+// Keep module.hot.accept for HMR - This is removed automatically on build
+if (module.hot) module.hot.accept();
+
